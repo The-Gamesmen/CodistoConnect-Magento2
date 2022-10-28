632474ac41163008572925607c246b56377bb403
Andrew Cattarin <andrew@gamesmen.com.au>
2022-10-29 07:25:13 +1100

    fix: return array with empty string instead of null to properly emulate pre-PHP8.1 behaviour of string operation on null

--- Model/Sync.php
+++ Model/Sync.php
@@ -2005,8 +2005,8 @@ class Sync
                 $attributeValue = $this->codistoHelper->processCmsContent($attributeValue, $storeId);
             }
         } elseif (in_array($attributeData['frontend_type'], ['select', 'multiselect'])) {
-            if($attributeData['frontend_type']=='multiselect' && !is_null($attributeValue)) {
-                $attributeValue = explode(',',$attributeValue);
+            if($attributeData['frontend_type']=='multiselect') {
+                $attributeValue = is_null($attributeValue) ? array('') : explode(',',$attributeValue);
             }
             $defaultValue = $this->_syncProductAttributeOptionText(
                 $attributeData['source'],
